2e2d89c47eb1e63f8e44c4a17afe0f851e5aeddd
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2022-01-28 21:23:35 -0500

    installed NextAuth for authentication to communicate with the spotify api for the login for the project. Paused on the sidebar component to get the api incorporated in the app.

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,7 +11,7 @@ function Sidebar() {
   return (
         <div className="text-gray-500 p-5 text-sm border-r
         border-gray-900">
-            <div>
+            <div className="space-y-4">
                 <button className="flex items-center space-x-2
                 hover:text-white">
                     <HomeIcon className="h-5 w-5"/>
@@ -44,8 +44,15 @@ function Sidebar() {
                     <RssIcon className="h-5 w-5"/>
                     <p>Your Episodes</p>
                 </button>
-                
                 <hr className="border-t-[0.1px] border-gray-900"/>
+
+                {/* playlist */}
+                <p className="cursor-pointer hover:text-white">Playlist Name</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name</p>
             </div>
         </div>
     )
