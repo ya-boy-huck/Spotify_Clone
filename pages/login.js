3e032de5a93bcb3e443b31c86a2d12c28440755e
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2022-02-07 12:19:56 -0500

    Added a higher order component in _app.tsx. To get user access token

--- pages/login.js
+++ pages/login.js
@@ -7,10 +7,12 @@ function Login() {
             <img className="w-52 mb-5" src="https://links.papareact.com/
             9xl" alt="" />
             
-            {Object.values(providers).map((provider) => (
+            {Object.values(providers).map((provider)=> (
                 <div key={provider.name}>
                     <button className="bg-[#18D860] text-white p-5
-                    rounded-full">
+                    rounded-full"
+                    onClick={() => signIn(provider.id, { callbackUrl: "/"})}
+                    >
                         Login with {provider.name}
                     </button>
                 </div>
