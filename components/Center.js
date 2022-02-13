f4b255fc9be7797af3c19c2eea6028d95580ac22
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2022-02-12 20:59:59 -0500

    Installed tailwind scrollbar hide package, to hide the scroll bar within the sidebar component that divided itself from Center component

--- components/Center.js
+++ components/Center.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 function Center() {
     return (
-        <div className="flex flex-grow">
+        <div className="flex flex-grow
+        text-white">
 
         </div>
     );
