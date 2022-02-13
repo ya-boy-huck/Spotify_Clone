6d6fab6f61c06c894dadc8f214c06c13612f7a37
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2022-02-12 21:39:11 -0500

    Installed Lodash packages

--- components/Center.js
+++ components/Center.js
@@ -1,8 +1,25 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
 import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+
+const colors = [
+    "from-indigo-500",
+    "from-blue-500",
+    "from-green-500",
+    "from-red-500",
+    "from-yellow-500",
+    "from-pink-500",
+    "from-purple-500",
+];
 
 function Center() {
     const { data: session } = useSession();
+    const [color, setColor] = useState(null);
+    
+    useEffect (() => {
+
+    }, [])
+
     return (
         <div className="flex-grow">
             <header className="absolute top-5 right-8">
@@ -17,7 +34,7 @@ function Center() {
                 </div>
             </header>
             <section className={`flex items-end space-x-7 bg-gradient-to-b to-black 
-            from-red-500 h-80 text-white padding-8 w-full`}>
+            from-red-500 h-80 text-white padding-8`}>
 
             </section>
         </div>
