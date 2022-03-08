eed3dbfb928fbc98f863ce5c27cb3c201cf4eb2e
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2022-03-08 11:35:27 -0500

    Loaded the users playlist with all the data (song, artist, album art and soon the duration of the song). Have proper spacing between the tracks to make it a clean UI

--- components/Center.js
+++ components/Center.js
@@ -37,7 +37,8 @@ function Center() {
     }, [spotifyApi, playlistId]);
 
     return (
-        <div className="flex-grow text-white">
+        <div className="flex-grow text-white h-screen overflow-y-scroll
+        scrollbar-hide">
             <header className="absolute top-5 right-8">
                 <div className="flex items-center bg-black space-x-3
                 opacity-90 hover:opacity-80 cursor-pointer rounded-full
--- components/Songs.js
+++ components/Songs.js
@@ -1,13 +1,14 @@
 import React from "react";
+import Song from "../components/Song";
 import { playlistState } from "../atoms/playlistAtoms";
 import { useRecoilValue } from "recoil";
 
 function Songs() {
     const playlist = useRecoilValue(playlistState);
     return (
-        <div className="text-white">
-            {playlist?.tracks.items.map((tracks) => (
-                <div>{tracks.track.name}</div>
+        <div className="text-white px-8 flex flex-col space-y-1 pb-26">
+            {playlist?.tracks.items.map((track, i) => (
+                <Song key={track.track.id} track={track} order={i} />
             ))}
         </div>
     )
